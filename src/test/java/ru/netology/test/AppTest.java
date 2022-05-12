90ee0cb600f9ab917ac4b2a5da2cfa82c4d781e0
zaksignu <zaksignu@gmail.com>
2022-05-12 12:01:54 +0300

    Final version

--- src/test/java/ru/netology/test/AppTest.java
+++ src/test/java/ru/netology/test/AppTest.java
@@ -1,11 +1,7 @@
 package ru.netology.test;
 
-//import lombok.var;
-
-
-import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
 import ru.netology.pages.DashBoardPage;
 import ru.netology.pages.LoginPage;
@@ -33,18 +29,6 @@ public class AppTest {
         verificationPage.validVerify(verificationCode);
     }
 
-    @Test
-    void shouldBlockAfterThreeTimeWrong() {
-        DataWizard.FellowOne ghostOne = DataWizard.Registr.generateUser();
-        DataWizard.userRegister(ghostOne);
-        open("http://localhost:9999");
-        var loginPage = new LoginPage();
-        var authInfoCorrect = DataWizard.getAuthInfo(ghostOne);
-        loginPage.invalidLogin(authInfoCorrect);
-        loginPage.invalidLogin(authInfoCorrect);
-        loginPage.invalidLogin(authInfoCorrect);
-        loginPage.invalidLogin(authInfoCorrect);
-        loginPage.validLogin(authInfoCorrect);
-    }
+
 
 }
